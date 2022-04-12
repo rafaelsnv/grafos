4d2e919e5899f7bb7701865556d396b08edf6cc7
Felipe Silva <felipe.barbosa1270@gmail.com>
2022-04-11 22:59:03 -0300

    gets e set classe vertice

--- src/Grafo/Vertice.java
+++ src/Grafo/Vertice.java
@@ -2,29 +2,61 @@ package Grafo;
 
 import java.util.ArrayList;
 
-public class Vertice
+public class Vertice {
+    public Vertice() {
 
-{
-    /** Attributes */
+    }
+
+    public Vertice(int id, int grau, ArrayList<Aresta> arestas) {
+        this.id = id;
+        this.grau = grau;
+        this.arestas = arestas;
+    }
+/** Attributes */
     /**
      * 
      */
     private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     /**
      * 
      */
     private int grau;
+    public int getGrau() {
+        return grau;
+    }
+
+    public void setgrau(int grau) {
+        this.grau = grau;
+    }
+
+
+
+
     /**
      * 
      */
     private ArrayList<Aresta> arestas;
-    /**
+
+    public ArrayList<Aresta> getArestas() {
+        return arestas;
+    }
+
+    public void setArestas(ArrayList<Aresta> arestas) {
+        this.arestas = arestas;
+    }
+/**
      * Operation getGrau
      *
      * @return int
      */
-    public int getGrau (  ){
-        return 0;
-    }
 }
 
